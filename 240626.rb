18a31f6bc0378c33aa977d0e4fd019f4e012a5eb
shirakurak <ken1shirakura@gmail.com>
2024-06-27 12:39:38 +0900

    up

--- 240626.rb
+++ 240626.rb
@@ -1,9 +1,14 @@
 # https://algo-logic.info/rec-bit-search/
+# 値が違う 𝑛 個の数字が与えられます。選び方をすべての場合について考えたとき
+# それぞれの場合で選んだ数値の和を、さらにすべて足して和を求めて下さい。
 
 list = [4, 10, 1]
 
+# リストの部分集合の全体の和を求める
 def bit_plus(list)
   sum = 0
+  # 1（ビット表現：0001）を左に3回シフトする（ビット表現：1000）
+  # 8（2^3）になるまで繰り返す
   (1 << list.length).times do |bit|
     list.length.times do |i|
       mask = 1 << i
